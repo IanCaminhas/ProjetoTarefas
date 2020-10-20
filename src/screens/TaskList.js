9c03865d2a946031a3ddc0be3952e2c9f0b368f1
IanCaminhas <iancaminhas25@hotmail.com>
2020-10-20 19:11:51 -0300

    Adicionando FlatList

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text, ImageBackground, StyleSheet} from  'react-native'
+import {View, Text, ImageBackground, StyleSheet, FlatList} from  'react-native'
 
 import commonStyles from '../CommonStyle'
 import todayImage from '../../assets/imgs/today.jpg'
@@ -11,6 +11,47 @@ import 'moment/locale/pt-br'
 
 
 export default class TaskList extends Component {
+
+    state ={
+        tasks:[{
+            id: Math.random(),
+            desc: 'Comprar Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: new Date(),
+        },{
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        },{
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        },{
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        },{
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        },{
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        },{
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }]
+    }
+
+
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
@@ -23,8 +64,9 @@ export default class TaskList extends Component {
                 </ImageBackground>
               
                 <View style={styles.taskList}>
-                    <Task desc="Comprar Livro" estimateAt={new Date()} doneAt={new Date()} />
-                    <Task desc="Ler Livro" estimateAt={new Date()} doneAt={null} />
+                    <FlatList data={this.state.tasks} 
+                              keyExtractor={item => `${item.id}`}
+                              renderItem={({item}) => <Task {...item} />}  />
                 </View>
 
             </View>
