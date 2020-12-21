7b266b2a305584ebf8ec085676ce6b729f3867d2
IanCaminhas <iancaminhas25@hotmail.com>
2020-12-21 07:52:08 -0300

    Integrando autenticacao com backend

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -8,29 +8,77 @@ import {
         Platform,
         Alert
 } from 'react-native' 
+import axios from 'axios'
+
+
+const initialState ={
+    name:'',
+    email:'',
+    password: '',
+    confirmPassword:'',
+    stageNew: false //atributo que diz se o usuário está logando ou criando novo usuário
+}
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../CommonStyle' 
 import AuthInput from '../components/AuthInput'
 
+import {server, showError, showSuccess} from '../common'
+
 export default class Auth extends Component {
     
     state ={
-        name:'',
-        email:'',
-        password: '',
-        confirmPassword:'',
-        stageNew: false //atributo que diz se o usuário está logando ou criando novo usuário
-
+        ...initialState
     }
     
     signOrSignup = () =>{
         if(this.state.stageNew){
-            Alert.Alert('Sucesso!', 'Criar conta')
+            this.signup()
         }else {
-            Alert.Alert('Sucesso!', 'Logar')
+            this.signin()
         }
     }
+
+    signup = async() =>{
+        try{
+            await axios.post(`${server}/signup`, {
+                name:this.state.name,
+                email:this.state.email,
+                password: this.state.password,
+                confirmPassword:this.state.confirmPassword,
+            })
+
+            //essa msg vai ser exibida quando o await for finalizado
+            showSuccess('Usuário cadastrado!')
+            //quando o usuario terminar de cadastrar, alterar para o estado inicial
+            this.setState({...initialState})
+            
+        }catch(e){
+            showError(e)
+        }
+    }
+
+
+    signin = async () =>{
+        try {
+            //vou precisar do resultado da resposta...guardo em res
+           const res= await axios.post(`${server}/signin`, {
+                email:this.state.email,
+                password: this.state.password,
+            })
+            //bearer(portador) -> toda requsição posterior terá o token 
+            //Essa String `bearer ${res.data.token}` está setada na header Authorization
+            //`bearer ${res.data.token}` authorization será enviada em qualquer nova requisição futura
+            axios.defaults.headers.common['Authorization']= `bearer ${res.data.token}`
+            
+            //caso a autenticação der certo, navegue para a tela Home
+            this.props.navigation.navigate('Home')
+        }catch(e){
+            //caso der erro, chame a exceção
+            showError(e)
+        }
+    }
+
 
     render(){
         return (
