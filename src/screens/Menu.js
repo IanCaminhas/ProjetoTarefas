3d77c5c1b0593910df26a1d67c0168e5d391f94c
IanCaminhas <iancaminhas25@hotmail.com>
2020-12-23 09:55:48 -0300

    Informacoes de usuario #2

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {Platform,ScrollView, View, Text, StyleSheet}from 'react-native'
 import {DrawerItems} from 'react-navigation-drawer'
 import {Gravatar} from 'react-native-gravatar'
-
+import commonStyles from '../CommonStyle' 
 
 export default props =>{
     return (
         //informações do usuário logado
         <ScrollView>
             <View style={styles.header}>
+                <Text style={styles.title}>Tasks</Text>
                 <Gravatar styles={styles.avatar} 
                 options={{
                     //lembra quando passei pelo navigator a resposta. Isso no method signin             
@@ -18,8 +19,12 @@ export default props =>{
                 }}/>   
 
                 <View style={styles.userInfo}>
-                    <Text>{props.navigation.getParam('name')}</Text>
-                    <Text>{props.navigation.getParam('email')}</Text>
+                    <Text style={styles.name}>
+                        {props.navigation.getParam('name')}
+                    </Text>
+                    <Text style={styles.email}>
+                        {props.navigation.getParam('email')}
+                    </Text>
                 </View> 
 
             </View>
@@ -33,13 +38,38 @@ const styles = StyleSheet.create({
         borderBottomWidth:1,
         borderColor:'#DDD'
     },
+    title:{
+        color:'#000',
+        fontFamily: commonStyles.fontFamily,
+        fontSize:30,
+        marginTop: Platform.OS ==='ios' ? 70 : 30,
+        padding:10
+        
+    },
     avatar:{
         width: 60,
         height: 60,
         borderWidth: 3,
         borderRadius:30,
         margin:10,
-        marginTop: Platform.OS ==='ios' ? 50 : 10
+        backgroundColor: '#222',
     },
+    userInfo:{
+        marginLeft:10,
+        flexDirection:'row'
+    },
+    name:{
+        fontFamily: commonStyles.fontFamily,
+        fontSize:20,
+        marginBottom: 5, //espaço entre o nome do usuário e o email
+        color: commonStyles.colors.Text
+
+    }, 
+    email:{
+        fontFamily: commonStyles.fontFamily,
+        fontSize:15,
+        color: commonStyles.colors.subText,
+        marginBottom: 10,
+    }
 
 })
