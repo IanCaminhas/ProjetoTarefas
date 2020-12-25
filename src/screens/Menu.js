bdeb9c98bcab7242b60ad2f74a63da59dbeeb525
IanCaminhas <iancaminhas25@hotmail.com>
2020-12-25 18:27:59 -0300

    Fazendo logout

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import {Platform,ScrollView, View, Text, StyleSheet}from 'react-native'
+import {Platform,ScrollView, View, Text, StyleSheet, TouchableOpacity}from 'react-native'
 import {DrawerItems} from 'react-navigation-drawer'
 import {Gravatar} from 'react-native-gravatar'
 import commonStyles from '../CommonStyle' 
+import axios from 'axios'
+import AsyncStorage from '@react-native-community/async-storage'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default props =>{
+
+    const logout =() =>{
+        //deletando o cabeçalho contendo o token
+        axios.defaults.headers.common['Authorization']
+        //remover registro no AsyncStorage
+        AsyncStorage.removeItem('userData')
+        //navegar para AuthOrApp quando fazer logout
+        props.navigation.navigate('AuthOrApp')
+    }
+
     return (
         //informações do usuário logado
         <ScrollView>
@@ -26,7 +39,12 @@ export default props =>{
                         {props.navigation.getParam('email')}
                     </Text>
                 </View> 
+                <TouchableOpacity onPress={logout}>
+                    <View style={styles.logoutIcon}>
+                        <Icon name='sign-out' size={30} color='#800' />
+                    </View>
 
+                </TouchableOpacity>
             </View>
             <DrawerItems {...props} />
         </ScrollView>
@@ -70,6 +88,12 @@ const styles = StyleSheet.create({
         fontSize:15,
         color: commonStyles.colors.subText,
         marginBottom: 10,
+    },
+    logoutIcon:{
+        marginLeft:10,
+        marginBottom: 10
+
     }
 
 })
+
